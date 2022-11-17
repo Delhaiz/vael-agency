e20639aaa4e834fc5c48661e802489a98082dd8c
Jonas Vael <s131632@ap.be>
2022-11-17 17:58:18 +0100

    Artist template page: opmaak en styling

--- src/pages/artists/{wpArtist.slug}.js
+++ src/pages/artists/{wpArtist.slug}.js
@@ -2,59 +2,116 @@ import * as React from "react"
 import { graphql } from "gatsby"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Layout from "../../components/layout"
-import ArtistsPage from "."
+import {
+  header,
+  headerInfo,
+  headerPicture,
+  artistName,
+  fullName,
+  artistRoles,
+  artistDescription,
+  artistInfo,
+  artistPictures,
+  artistPicture,
+} from "../../page.module.css"
 
 const ArtistPage = ({
   data: {
-    wpArtist: { artistMeta: artist },
+    wpArtist: {
+      artistMeta: artist,
+      roles: { nodes: roles },
+    },
   },
 }) => {
   const image = getImage(artist.profilePicture.localFile)
+  const image1 = getImage(artist.picture1.localFile)
+  const image2 = getImage(artist.picture2.localFile)
+  const image3 = getImage(artist.picture3.localFile)
   return (
     <Layout pageTitle="Artist Template">
-      <div>
-        <GatsbyImage image={image} alt={artist.profilePicture.altText} />
-        <h3>{artist.artistName}</h3>
-        <h1>
-          {artist.firstName} {artist.lastName}
-        </h1>
-        <div dangerouslySetInnerHTML={{ __html: artist.description }} />
-        <p>Email: {artist.email}</p>
-        <p>Phone: {artist.phone}</p>
-        <p>Height: {artist.height}</p>
-        <p>Shirt Size: {artist.shirtSize}</p>
-        <p>Shoe Size: {artist.shoeSize}</p>
-        <p>Origin: {artist.origin}</p>
-      </div>
+      // Header JSX
+      <section className={artistPictures}>
+       {image1 && (
+          <GatsbyImage
+            className={artistPicture}
+            image={image1}
+            alt={artist.picture1.altText}
+          />
+        )}
+        {image2 && (
+          <GatsbyImage
+            className={artistPicture}
+            image={image2}
+            alt={artist.picture2.altText}
+          />
+        )}
+        {image3 && (
+          <GatsbyImage
+            className={artistPicture}
+            image={image3}
+            alt={artist.picture3.altText}
+          />
+        )}
+      </section>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query MyQuery($slug: String) {
-    wpArtist(slug: { eq: $slug }) {
-      artistMeta {
-        firstName
-        lastName
-        email
-        description
-        artistName
-        height
-        origin
-        phoneNumber
-        shirtSize
-        shoeSize
-        profilePicture {
-          localFile {
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED)
-            }
+query ($slug: String) {
+  wpArtist(slug: {eq: $slug}) {
+    artistMeta {
+      firstName
+      lastName
+      email
+      description
+      artistName
+      height
+      origin
+      phoneNumber
+      shirtSize
+      shoeSize
+      profilePicture {
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
           }
-          altText
+        }
+        altText
+      }
+      picture1 {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+      picture2 {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+      }
+      picture3 {
+        altText
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED)
+          }
         }
       }
     }
+    roles {
+      nodes {
+        name
+      }
+    }
   }
+}
+
 `
 
 export default ArtistPage
