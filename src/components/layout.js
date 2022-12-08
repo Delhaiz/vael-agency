82b07edd7a29b1f1eeb9dd84d9eda9fd62a600d3
Jonas Vael <s131632@ap.be>
2022-12-08 14:15:43 +0100

    Contact page: opmaak en styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -33,27 +33,31 @@ const Layout = ({ children }) => {
     <>
     <div className={container}>
       <title>{data.site.siteMetadata.title}</title>
-      <nav className={nav}>
-        <header className={siteTitle}>{data.site.siteMetadata.title}</header>
-        <ul className={navLinks}>
-          <li></li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/">
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/about">
-              About
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link className={navLinkText} to="/artists">
-              Artists
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <nav className={nav}>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      <ul className={navLinks}>
+        <li className={navLinkItem}>
+          <Link className={navLinkText} to="/">
+            Home
+          </Link>
+        </li>
+        <li className={navLinkItem}>
+          <Link className={navLinkText} to="/about">
+            About
+          </Link>
+        </li>
+        <li className={navLinkItem}>
+          <Link className={navLinkText} to="/artists">
+            Artists
+          </Link>
+        </li>
+        <li className={navLinkItem}>
+          <Link className={navLinkText} to="/contact">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
       <main>{children}</main>
     </div>
     <Footer siteTitle={data.site.siteMetadata.title} companyInfo={data.wpPage.contactUsFields}/>
